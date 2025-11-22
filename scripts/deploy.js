64a0bb1a58ad265dbdac2e12ac895a1fd309e005
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-11-22 14:26:03 +0900

    Script to deploy the contract

---
+++ scripts/deploy.js
@@ -0,0 +1,19 @@
+import { ethers } from "hardhat";
+
+async function main() {
+    console.log("🚀 Deploying AccessControlSecurity contract...");
+
+    await hre.run("compile");
+
+    const AccessControlSecurity = await ethers.deployContract("AccessControlSecurity");
+    console.log("⏳ Waiting for deployment confirmation...");
+    await AccessControlSecurity.waitForDeployment();
+
+    const deployedAddress = await AccessControlSecurity.getAddress();
+    console.log(`✅ AccessControlSecurity deployed at: ${deployedAddress}`)
+}
+
+main().catch((error) => {
+    console.error("❌ Deployment failed:", error);
+    process.exitCode = 1;
+});
